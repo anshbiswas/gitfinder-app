76ede36bdd2fac0b7b744bbc9dcc4dba93d0f896
anshbiswas <65728642+anshbiswas@users.noreply.github.com>
2022-02-06 21:01:48 +0530

    initial

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -12,8 +12,7 @@ export const searchUsers = async (text) => {
     const params = new URLSearchParams({
             q: text,
         })
-
-    const response = await github.get(`/search/users/?${params}`)
+    const response = await github.get(`/search/users?${params}`)
     return response.data.items
 }
 
